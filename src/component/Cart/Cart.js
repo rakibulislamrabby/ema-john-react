94dacf32bd1fb564c2b766a6861ca6ff0e1740c1
Rakibulislamrabby <rakibulislamrabby64@gmai.com>
2022-03-24 13:58:52 +0600

    update

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Cart.css'
-const Cart = ({ cart }) => {
+const Cart = (props) => {
+    const { cart } = props;
+    console.log(cart);
+    let Total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        Total = Total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax = (Total * 10) / 100;
+    const grandTotal = Total + shipping + tax;
     return (
-        <div>
+        <div className='cart'>
             <h1 className='order-summary'>Order Summary</h1>
-            <p>Selected Cart:{cart.length}</p>
+            <p>Selected Cart: ${cart.length}</p>
+            <p>Total Price: ${Total}</p>
+            <p>Total shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <p><b>Grand Total: ${grandTotal}</b></p>
         </div>
     );
 };
